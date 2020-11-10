5c069da120984510e0f8be1954db8882047c2231
erobert <etiennerobert33@gmail.com>
2020-11-10 02:07:54 +0100

    add some css

--- src/my-app.ts
+++ src/my-app.ts
@@ -1,7 +1,29 @@
-import { LitElement, html, TemplateResult, customElement } from 'lit-element';
+import {
+  LitElement,
+  html,
+  TemplateResult,
+  customElement,
+  css,
+} from 'lit-element';
 
 @customElement('my-app')
 class MyApp extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        display: grid;
+        place-content: center;
+        height: 100%;
+        background: lightpink;
+      }
+
+      h1 {
+        margin: 0;
+        font-size: 4rem;
+      }
+    `;
+  }
+
   render(): TemplateResult {
     return html`<h1>Hello World!</h1>`;
   }
